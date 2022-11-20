539041efd1ffb1c16f17f3c5e1c523f221e67b1e
cnmy-ro <chinmay.srao97@gmail.com>
2022-11-20 21:22:39 +0100

    Add NN univeral approx demo

--- prototype_nd.py
+++ prototype_nd.py
@@ -17,7 +17,7 @@ class Variable:
         if self.prev is not None:
 
             fn, fn_vars = self.prev[0], self.prev[1]
-            fn_vars_vjp = fn.vjp(grad, self, *fn_vars)
+            fn_vars_vjp = fn.vjp(self.grad, self, *fn_vars)
             
             for i in range(len(fn_vars)):
                 fn_var_grad = fn_vars_vjp[i]
@@ -41,17 +41,20 @@ class Variable:
     def __pow__(self, other):
         return Pow()(self, other)
 
+    def dot(self, other):
+        return Dot()(self, other)
+
     def sum(self):
         return Sum()(self)
 
     def relu(self):
         return ReLU()(self)
 
+    def sigmoid(self):
+        return Sigmoid()(self)
+
     def tanh(self):
         return Tanh()(self)
-
-    def dot(self, other):
-        return Dot()(self, other)
 
 
 class Function:
@@ -67,8 +70,8 @@ class Function:
 
     def vjp(self, grad: np.ndarray, result: Variable, *args: Variable) -> list[np.ndarray]:
         """
-        Vector-Jacobian product
-        Implicitly computes J-dot-grad without having to materialize the massive J
+        Vector-Jacobian product.
+        Implicitly computes J-dot-grad without having to materialize the massive J.
         """
         ...
 
@@ -80,33 +83,6 @@ class Neg(Function):
 
     def vjp(self, grad, result, x):
         return [-grad]
-
-
-class Sum(Function):
-
-    def fx(self, x):
-        return Variable(x.data.sum())
-
-    def vjp(self, grad, result, x):
-        return [grad]
-
-
-class ReLU(Function):
-
-    def fx(self, x):
-        return Variable(np.maximum(x.data, np.zeros_like(x.data)))
-
-    def vjp(self, grad, result, x):
-        return [grad * (result > 0).astype(float)]
-
-
-class Tanh(Function):
-
-    def fx(self, x):
-        return Variable(np.tanh(x.data))
-
-    def vjp(self, grad, result, x):
-        return [grad * (1 - np.tanh(x.data)**2)]
 
 
 class Add(Function):
@@ -142,16 +118,16 @@ class Div(Function):
         return Variable(x1.data / x2.data)
 
     def vjp(self, grad, result, x1, x2):
-        return [grad * (1.0 / x2.data), grad * x1.data * (-1.0 / (x2.data**2))]
+        return [grad * (1. / x2.data), grad * x1.data * (-1. / (x2.data**2))]
 
 
 class Pow(Function):
 
     def fx(self, x1, x2):
-        return Variable(x1.data ** x2)
+        return Variable(x1.data ** x2.data)
 
     def vjp(self, grad, result, x1, x2):
-        return [grad * x2 * x1.data ** (x2 - 1)]
+        return [grad * x2.data * x1.data ** (x2.data - 1.), grad * np.log(x1.data) * x1.data ** x2.data]
 
 
 class Dot(Function):
@@ -160,52 +136,134 @@ class Dot(Function):
         return Variable(x1.data @ x2.data)
 
     def vjp(self, grad, result, x1, x2):
-        return [None, None]
+        return [grad @ x2.data.T, x1.data.T @ grad]
+
+
+class Sum(Function):
+
+    def fx(self, x):
+        return Variable(x.data.sum())
+
+    def vjp(self, grad, result, x):
+        return [grad]
+
+
+class ReLU(Function):
+
+    def fx(self, x):
+        return Variable(np.maximum(x.data, np.zeros_like(x.data)))
+
+    def vjp(self, grad, result, x):
+        return [grad * (result.data > 0.0).astype(float)]
+
+
+class Sigmoid(Function):
+
+    def _sigma(self, x):
+        return 1. / (1. + np.exp(-x))
+
+    def fx(self, x):
+        return Variable(self._sigma(x.data))
+
+    def vjp(self, grad, result, x):
+        return [grad * self._sigma(x.data) * (1. - self._sigma(x.data))]
+
+
+class Tanh(Function):
+
+    def fx(self, x):
+        return Variable(np.tanh(x.data))
+
+    def vjp(self, grad, result, x):
+        return [grad * (1. - np.tanh(x.data)**2)]
 
 
 
 if __name__ == '__main__':
 
+    import time
     import matplotlib.pyplot as plt
+    from tqdm import tqdm
 
     def run_demo():
 
         """ App demonstrating the universal approximation capability of an NN. """
         
-        xtrain, ytrain = None
-        xtest, ytest = None
+        xtrain = np.random.rand(1000) * 2 * np.pi
+        noise = np.random.normal(0, 0.01, size=(1000,))
+        ytrain = np.sin(xtrain) + noise
+
+        xtest = np.linspace(0, 2*np.pi, 1000)
+        ytest = np.sin(xtest)
+
+        # Dummy value "1" to use with learnable bias
+        xtrain = np.stack((xtrain, np.ones_like(xtrain)), axis=1)
+        xtest = np.stack((xtest, np.ones_like(xtest)), axis=1)
 
         def mlp(x, params):
             z1 = params['w1'].dot(x)
-            a1 = x.relu()
+            a1 = z1.sigmoid()
             z2 = params['w2'].dot(a1)
-            y = z2.tanh()            
+            y = z2.tanh()          
             return y
 
         def init_params():
-            params = {'w1': None, 'w2': None}
+            params = {
+            'w1': Variable(np.random.normal(size=(8, 2))),
+            'w2': Variable(np.random.normal(size=(1, 8)))
+            }
             return params
 
         def update_params(params, lr):
-            params = {k: v - lr*v.grad for k,v in params.items()}
+            for k in params.keys():
+                params[k].data = params[k].data - lr*params[k].grad
             return params
 
         def zero_grad(params):
-            params = {k: v.grad = np.zeros_like(v.data) for k,v in params.items()}
+            for k in params.keys():
+                params[k].grad = np.zeros_like(params[k].data)
             return params
 
-        for e in range(1, 10):
-                        
+        def test(params, xtest, ytest):
+            ytestpred = []
+            for i in range(xtest.shape[0]):
+                x_i = np.expand_dims(xtest[i] / np.pi - 1., axis=1)
+                z1 = np.dot(params['w1'].data, x_i)
+                a1 = 1 / (1 + np.exp(-z1))
+                z2 = np.dot(params['w2'].data, a1)
+                ypred_i = np.tanh(z2)
+                ytestpred.append(np.squeeze(ypred_i))
+            ytestpred = np.array(ytestpred)
+
+            return ytestpred
+    
+        params = init_params()
+        ytestpred = test(params, xtest, ytest)
+        
+        fig, ax = plt.subplots()
+        testgt_plot = plt.plot(xtest[:,0], ytest, c='tab:blue')[0]
+        testpred_plot = plt.plot(xtest[:,0], ytestpred, c='tab:orange')[0]
+        plt.ion()
+        plt.show()
+
+        for e in tqdm(range(1, 50)):
+
             for i in range(xtrain.shape[0]):
                 
-                x_i = Variable(xtrain[i])
-                ytrue_i = Variable(ytrain[i])
+                x_i = Variable(np.expand_dims(xtrain[i] / np.pi - 1., axis=1))
+                ytrue_i = Variable(np.expand_dims(np.expand_dims(ytrain[i], axis=0), axis=0))
                 ypred_i = mlp(x_i, params)
 
-                loss = (ypred_i - ytrue_i) ** 2
+                loss = (ypred_i - ytrue_i) ** Variable(np.array([[2.]]))
                 loss.backward()
-                params = update_params(params)
-                params = zero_grad(params)
+                params = update_params(params, lr=0.5)
+                params = zero_grad(params)                        
 
-            # Viz
-            # ...
+            ytestpred = test(params, xtest, ytest)
+            testpred_plot.set_ydata(ytestpred)
+            fig.canvas.draw()
+            time.sleep(0.005)
+            fig.canvas.flush_events()    
+
+
+    run_demo()
